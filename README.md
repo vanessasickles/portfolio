ed57dc6e1e6894d8eaa0e1256bd9d3b4e2072c72
vanessasickles <vsicklesshs@gmail.com>
2020-02-04 21:23:01 -0600

    Set up Webpack, PostCSS, watcher

---
+++ README.md
@@ -0,0 +1,6 @@
+# URPG WordPress Theme - 2020
+This is the repository for the building of the new pokemonurpg.com WordPress theme.
+
+## Setup (WIP)
+1. Clone this repository to your desired folder using [Git](https://git-scm.com/), or a GUI for Git such as the [GitHub Desktop](https://desktop.github.com/) client.
+2. Set up your local environment through your desired method. I use [MAMP](https://www.mamp.info/en/mamp/), but you may use whatever you prefer.
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      gold: '#C6A521',
+      blue: {
+        'dark': '#003C5A',
+        'default': '#0475AE',
+      },
+      grey: {
+        'dark': '#474747',
+        'default': '#707070',
+        'light': '#BABABA',
+        'lighter': '#F5F5F5'
+      }
+    },
+
+    extend: {
+      fontFamily: {
+        'sans': ['Nunito', 'Arial']
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
